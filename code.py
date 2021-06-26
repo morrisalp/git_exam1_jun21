e7add2781138352d0bb47e1d232734f923a00a9b
Ploni Almoni <ploni@itc.tech>
2021-06-26 14:58:19 +0300

    add code to show random greeting

---
+++ code.py
@@ -0,0 +1,12 @@
+from random import random
+
+def main():
+    r = random()
+    if r < 0.5:
+        print("Hello! Nice to see you.")
+    else:
+        print("Hi! How are you?")
+
+
+if __name__ == "__main__":
+    main()
